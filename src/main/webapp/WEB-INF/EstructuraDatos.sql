ba454dd39ab34941d6244888ac57e38b1ce88a05
AngelIvanOC <20233tn086@utez.edu.mx>
2024-07-21 17:32:32 -0600

    Arreglo Base Aspirantes

--- src/main/webapp/WEB-INF/EstructuraDatos.sql
+++ src/main/webapp/WEB-INF/EstructuraDatos.sql
@@ -64,7 +64,6 @@ CREATE TABLE `aspirantes` (
                               PRIMARY KEY (`folio_aspirante`),
                               KEY `estado_id_estado` (`estado`),
                               KEY `grupos_id_grupo` (`grupos_id_grupo`),
-                              CONSTRAINT `aspirantes_ibfk_1` FOREIGN KEY (`estado`) REFERENCES `estado` (`id_estado`),
                               CONSTRAINT `aspirantes_ibfk_2` FOREIGN KEY (`grupos_id_grupo`) REFERENCES `grupos` (`id_grupo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
