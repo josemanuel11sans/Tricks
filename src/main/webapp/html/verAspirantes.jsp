3eb2641f9b84a2c95109f48a804ca84f5aaa995f
AngelIvanOC <20233tn086@utez.edu.mx>
2024-07-26 13:52:54 -0600

    js Aspirantes

--- src/main/webapp/html/verAspirantes.jsp
+++ src/main/webapp/html/verAspirantes.jsp
@@ -351,7 +351,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="../js/jsAspirantes.js"></script>
+<script src="../js/verAspirantes.js"></script>
 <script src="../js/script.js"></script>
 <script src="../js/scriptAlertas.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
--- src/main/webapp/js/verAspirantes.js
+++ src/main/webapp/js/verAspirantes.js
@@ -1,43 +1,75 @@
-function obtenerAspirantesDeLocalStorage() {
-  const aspirantesString = localStorage.getItem("aspirantes");
-  return aspirantesString ? JSON.parse(aspirantesString) : [];
+<!-- Script para abrir la imagen de carga masiva de ejemplo -->
+function openImagePopup(url, title) {
+    var width = 800;
+    var height = 600;
+    var left = (screen.width - width) / 2;
+    var top = (screen.height - height) / 2;
+    window.open(url, title, 'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
 }
-function guardarAspirantesEnLocalStorage(aspirantes) {
-  localStorage.setItem("aspirantes", JSON.stringify(aspirantes));
-}
-function actualizarTabla(aspirantes) {
-  let aspirantesTableBody = document.getElementById("aspirantesTableBody");
-  aspirantesTableBody.innerHTML = "";
-  aspirantes.forEach((aspirante, index) => {
-    const newRow = document.createElement("tr");
-    newRow.innerHTML = `
-          <td>${aspirante.nombre}</td>
-          <td>${aspirante.apellidos}</td>
-          <td>${aspirante.curp}</td>
-          <td>${aspirante.folio}</td>
-          <td>${aspirante.fechaNacimiento}</td>
-          <td>
-            <button class="btn btn-danger btn-eliminar" data-index="${index}">
-              <i class="fas fa-trash"></i>
-            </button>
-          </td>
-          <td>
-            <a href="registrarAspirantes.jsp?index=${index}" class="btn btn-primary">
-              <i class="fas fa-edit"></i>
-            </a>
-          </td>
-      `;
-    aspirantesTableBody.appendChild(newRow);
-  });
-  let btnsEliminar = document.querySelectorAll(".btn-eliminar");
-  btnsEliminar.forEach((btn) => {
-    btn.addEventListener("click", function () {
-      let index = parseInt(btn.getAttribute("data-index"));
-      aspirantes.splice(index, 1);
-      guardarAspirantesEnLocalStorage(aspirantes);
-      actualizarTabla(aspirantes);
+document.addEventListener('DOMContentLoaded', function () {
+    var filterName = document.getElementById('filterName');
+    var filterCareer = document.getElementById('filterCareer');
+    var filterDivision = document.getElementById('filterDivision');
+
+    filterName.addEventListener('input', filterTable);
+    filterCareer.addEventListener('change', filterTable);
+    filterDivision.addEventListener('change', filterTable);
+
+    function filterTable() {
+        var filterNameValue = filterName.value.toLowerCase();
+        var filterCareerValue = filterCareer.value.toLowerCase();
+        var filterDivisionValue = filterDivision.value.toLowerCase();
+        var table = document.getElementById('example');
+        var rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+
+        for (var i = 0; i < rows.length; i++) {
+            var cells = rows[i].getElementsByTagName('td');
+            var folio = cells[0].textContent.toLowerCase();  // Columna "Folio"
+            var name = cells[1].textContent.toLowerCase();   // Columna "Nombre"
+            var curp = cells[2].textContent.toLowerCase();   // Columna "CURP"
+            var division = cells[3].textContent.toLowerCase(); // Columna "Grupo"
+            var career = cells[4].querySelector('div').getAttribute('data-estado'); // Columna "Estado"
+
+            var nameMatch = filterNameValue === '' || folio.includes(filterNameValue) || name.includes(filterNameValue) || curp.includes(filterNameValue);
+            var careerMatch = filterCareerValue === '' || career === filterCareerValue;
+            var divisionMatch = filterDivisionValue === '' || division === filterDivisionValue;
+
+            if (nameMatch && careerMatch && divisionMatch) {
+                rows[i].style.display = '';
+            } else {
+                rows[i].style.display = 'none';
+            }
+        }
+    }
+
+    // Añadir evento a los botones para capturar el valor del folio y el estado
+    document.querySelectorAll('.btn-modificar, .activo, .inactivo').forEach(function(button) {
+        button.addEventListener('click', function() {
+            var folio = this.closest('tr').getAttribute('data-folio');
+            document.getElementById('folioAspirante2').value = folio;
+
+            var estadoActual = this.getAttribute('data-estado');
+            var estadoContrario = estadoActual === '1' ? '2' : '1';
+            document.getElementById('estadoAspirante').value = estadoContrario;
+
+        });
     });
-  });
-}
-let aspirantes = obtenerAspirantesDeLocalStorage();
-actualizarTabla(aspirantes);
+
+    // Evento para modal de actualizar aspirante
+    document.querySelectorAll('.btn-modificar').forEach(function(button) {
+        button.addEventListener('click', function() {
+            var folio = button.getAttribute('data-folio');
+            var nombre = button.getAttribute('data-nombre');
+            var apellido = button.getAttribute('data-apellido');
+            var curp = button.getAttribute('data-curp');
+            var fecha = button.getAttribute('data-fecha');
+
+            document.getElementById('folioAspirante3').value = folio;
+            document.getElementById('nombreAspirante3').value = nombre;
+            document.getElementById('apellidosAspirante3').value = apellido;
+            document.getElementById('curpAspirante3').value = curp;
+            document.getElementById('fechaNacimientoAspirante3').value = fecha;
+            console.log(fecha);
+        });
+    });
+});
